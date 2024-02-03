f707980045684a836f4df916977c0fe4795605ed
Juan <j.tejeda46@yahoo.com>
2024-02-03 10:00:12 -0600

    Leetcode 128. Longest Consecutive Sequence

---
+++ Arrays and Hashing/longestConsecSq.py
@@ -0,0 +1,32 @@
+# 128. Longest Consecutive Sequence
+# Medium
+
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+# You must write an algorithm that runs in O(n) time.
+
+# Example 1:
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+# Example 2:
+# Input: nums = [0,3,7,2,5,8,4,6,0,1]
+# Output: 9
+nums = [100,4,200,1,3,2]
+
+def longestConsecutiveSequence(nums):
+    # Find the start of a sequence
+    hashset = set(nums)
+    res = 0
+    
+    for num in hashset:
+        if (num - 1) not in hashset:
+            length = 1
+            while num + length in hashset:
+                length += 1
+            res = max(res, length)
+    return res
+
+
+print(longestConsecutiveSequence(nums))
