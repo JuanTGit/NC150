7a9a82e9b83c548b9c2e423fc2bdaa0b60b176a7
Juan <j.tejeda46@yahoo.com>
2024-02-05 14:39:50 -0600

    Leetcode 11. Container With Most Water

--- Two Pointers/containerMostWater.py
+++ Two Pointers/containerMostWater.py
@@ -10,7 +10,7 @@
 # Notice that you may not slant the container.
 
 # Example 1:
-# Input: height = [1,8,6,2,5,4,8,3,7]
+# Input: height = 1
 # Output: 49
 # Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, 
 # the max area of water (blue section) the container can contain is 49.
@@ -20,4 +20,21 @@
 # Output: 1
 
 def maxArea(height):
-    pass
+    res = 0
+
+    l,r = 0, len(height) - 1
+
+    while l <= r:
+        res = max(res, (r-l) * min(height[l], height[r]))
+        if height[l] > height[r]:
+            r -= 1
+        elif height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+            l += 1
+    return res
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))
+
+
